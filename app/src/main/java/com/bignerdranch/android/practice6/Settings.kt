cb1abf78ba4b865d61c9bef00809fca20642ad64
_codefix <ivangorbatov91@gmail.com>
2023-04-03 21:52:37 +0500

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/practice6/Settings.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.practice6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
